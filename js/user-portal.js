689b74e2d36b73c5d76ce5d7d51b2aa556749d67
Amdadul Haq <123226006+Amdadulhaq@users.noreply.github.com>
2025-01-02 18:07:36 -0500

    Update user-portal.js

--- js/user-portal.js
+++ js/user-portal.js
@@ -14,13 +14,13 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA2LC916BFUO-LHC25Gek0y595GxFQA0ds",
-    authDomain: "house-rentals-12c7d.firebaseapp.com",
-    projectId: "house-rentals-12c7d",
-    storageBucket: "house-rentals-12c7d.firebaseapp.com",
-    messagingSenderId: "38104073059",
-    appId: "1:38104073059:web:19f5fe83b6601f29474956",
-    measurementId: "G-54J13NNWH7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
